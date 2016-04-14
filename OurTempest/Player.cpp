cb1941399d72b2ccc6339d296907fd97475e87b9
BrokenProgrammer <telwa.1000@hotmail.com>
2016-04-15 04:13:47 +0800

    Begun work on proper player controls

--- OurTempest/Player.cpp
+++ OurTempest/Player.cpp
@@ -5,7 +5,28 @@
 //#include <QDebug>
 Player::Player() : MovingGameObj()
 {
-	setRect(200, 350, 100, 100);
+	//Set position
+	position.x = 0;
+	position.y = 0;
+
+	//Set values for player variables
+	velocity.x = 5;
+	velocity.y = 5;
+	velocityMax.x = 10;
+	velocityMax.y = 10;
+	direction.x = 0;
+	direction.y = 0; //Go downwards
+	size.x = 100;
+	size.y = 100;
+	//qDebug() << pos();
+	//qDebug() << scenePos();
+
+	//setRect(250, 350, 100, 100);
+	//TODO: make enemy in the shape of a romb
+	setRect(position.x, position.y, size.x, size.y);
+
+	// Gör att fienden inte "driftar åt sidan utan förblir stationär när den skalas upp
+	//setPos(250, 350);
 }
 
 //TODO::Paint player just for fun
@@ -19,26 +40,32 @@ Player::Player() : MovingGameObj()
 
 void Player::keyPressEvent(QKeyEvent * e)
 {
+	isKeyPressed = true;
+	//keys[e->key()] = true; 
+	//QGraphicsRectItem::keyPressEvent(e);
+
 	//qDebug() << "Player knows that you pressed a key";
+	/*direction.x = 0;
+	direction.y = 0;
 
 	if (e->key() == Qt::Key_A)
 	{
-		setPos(x() - 10, y());
+		direction.x = -1;
 	}
 
-	if (e->key() == Qt::Key_D)
+	else if (e->key() == Qt::Key_D)
 	{
-		setPos(x() + 10, y());
+		direction.x = 1;
 	}
 
 	if (e->key() == Qt::Key_W)
 	{
-		setPos(x(), y() - 10);
+		direction.y = -1;
 	}
 
-	if (e->key() == Qt::Key_S)
+	else if (e->key() == Qt::Key_S)
 	{
-		setPos(x(), y() + 10);
+		direction.y = 1;
 	}
 
 	if (e->key() == Qt::Key_Space)
@@ -49,7 +76,48 @@ void Player::keyPressEvent(QKeyEvent * e)
 		bullet->setPos(x(), y() + 10);
 		//scene()->addItem(bullet);
 	}
+
+	updateVelocity();
+	setPos(position.x, position.y);*/
 }
+
+void Player::keyReleaseEvent(QKeyEvent * e)
+{
+	isKeyPressed = false;
+	//keys[e->key()] = false; 
+	//QGraphicsRectItem::keyReleaseEvent(e);
+
+	//direction.x = 0;
+	//direction.y = 0;
+}
+
+/*void Player::timerEvent(QTimerEvent *)
+{
+	//if (keys[Qt::Key_Up])
+	if (keys[Qt::Key_W])
+		direction.y = -1;
+
+	if (keys[Qt::Key_S])
+		direction.y = 1;
+
+	if (keys[Qt::Key_A])
+		direction.x = -1;
+
+	if (keys[Qt::Key_D])
+		direction.x = 1;
+
+	if (keys[Qt::Key_Space])
+	{
+		//Skapar en bullet
+		Bullet * bullet = new Bullet();
+		//qDebug() << "Player knows you want to kill";
+		bullet->setPos(x(), y() + 10);
+		//scene()->addItem(bullet);
+	}
+
+	//updateVelocity();
+	//setPos(position.x, position.y); 
+}*/
 
 Player::~Player()
 {
--- OurTempest/Player.h
+++ OurTempest/Player.h
@@ -13,13 +13,18 @@ class Player : public MovingGameObj //,QGraphicsItem
 		Player();
 		~Player();
 
-	//void paint (QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget = 0);
-	//void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) Q_DECL_OVERRIDE;
+		//void paint (QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget = 0);
+		//void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) Q_DECL_OVERRIDE;
 
-	void keyPressEvent(QKeyEvent *e);
+		void keyPressEvent(QKeyEvent *e);
+		void keyReleaseEvent(QKeyEvent *e);
+		//void timerEvent(QTimerEvent *);
 
 	private:
+		bool isKeyPressed;
 		//Player *_player1;
+		//QMap<int, bool> keys;
+
 };
 
 #endif //PLAYER_H
